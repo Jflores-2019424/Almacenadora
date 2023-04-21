f867055a387c028d061d01806c4fac37473131f3
lvannn <107076135+lvannn@users.noreply.github.com>
2023-04-21 10:12:33 -1200

    front-end
    creación del front-end del proyecto.

---
+++ react-alamacenadora/src/listaTareas/components/ListaTareas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ListaTareas = () => {
+  return (
+    <>
+    <div>
+        <h1>HOLA MUNDO</h1>
+    </div>
+    </>
+  )
+}
---
+++ react-alamacenadora/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ListaTareas } from './listaTareas/components/ListaTareas'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ListaTareas/>
+  </React.StrictMode>,
+)
