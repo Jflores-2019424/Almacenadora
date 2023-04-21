5727f36bfd22226cc3bde147ea59e07e91f2e980
lvannn <107076135+lvannn@users.noreply.github.com>
2023-04-21 10:19:50 -1200

    error
    ese no es

--- react-alamacenadora/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ListaTareas } from './listaTareas/components/ListaTareas'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ListaTareas/>
-  </React.StrictMode>,
-)
